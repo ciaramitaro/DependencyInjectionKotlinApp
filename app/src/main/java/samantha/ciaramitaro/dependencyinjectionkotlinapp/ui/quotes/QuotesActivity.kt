472607d8417a0f23ccc3eaa6d56c34b9259052b9
ciaramitaro <s.n.ciaramitaro@gmail.com>
2019-05-25 06:47:44 -0500

    established MVVM project structure setup

---
+++ app/src/main/java/samantha/ciaramitaro/dependencyinjectionkotlinapp/ui/quotes/QuotesActivity.kt
@@ -0,0 +1,13 @@
+package samantha.ciaramitaro.dependencyinjectionkotlinapp.ui.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import samantha.ciaramitaro.dependencyinjectionkotlinapp.R
+
+class QuotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
